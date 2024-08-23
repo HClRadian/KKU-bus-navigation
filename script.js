88e136b3169c5582cca1dc44c7b01c25d5a215f9
HClRadian <Chakkapat.d@kkumail.com>
2024-08-24 05:26:58 +0700

    Update script.js

--- script.js
+++ script.js
@@ -109,11 +109,11 @@ for (var key in stations) {
 }
 
 function calculateRoute() {
-    var start = document.getElementById("หอพัก 8 หลัง").value;
-    var end = document.getElementById("คณะเกษตรศาสตร์2").value;
+    var start = document.getElementById("start").value;
+    var end = document.getElementById("end").value;
 
-    var startCoords = stations[หอพัก 8 หลัง];
-    var endCoords = stations[คณะเกษตรศาสตร์2];
+    var startCoords = stations[start];
+    var endCoords = stations[end];
   if (startCoords && endCoords) {
         // เพิ่มเส้นทางในแผนที่
         L.Routing.control({
