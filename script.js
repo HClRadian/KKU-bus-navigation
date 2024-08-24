6c11ef5cad12af68e201817653ae076e12c29573
HClRadian <Chakkapat.d@kkumail.com>
2024-08-24 13:57:42 +0700

    Update script.js

--- script.js
+++ script.js
@@ -99,7 +99,7 @@ var stations = {
 };
 
 // จุดที่บังคับให้ผ่าน
-var mandatoryStops = ["ที่พักญาติ", "คณะเกษตรศาสตร์2"];
+var mandatoryStops = ["ที่พักญาติ", "คณะเกษตรศาสตร์2","คณะเทศโนโลยี1"];
 
 for (var key in stations) {
     L.marker(stations[key]).addTo(map).bindPopup(key);
